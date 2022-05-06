70cd62b6d2289781dcc3534c7d1de5c99983ce0e
Gabriel P <60277324+gabrielp-oliveira@users.noreply.github.com>
2022-05-05 23:12:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const bodyParser = require('body-parser')
 
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 require('./controllers/authController')(app)
